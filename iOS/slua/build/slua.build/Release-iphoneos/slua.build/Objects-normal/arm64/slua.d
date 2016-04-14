a28c18f8b2e74d418dccca4019c0700b30c1c2bb
zhukunqian <zhukunqian@gmail.com>
2016-04-14 15:34:22 +0800

    add

--- iOS/slua/build/slua.build/Release-iphoneos/slua.build/Objects-normal/arm64/slua.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/sanx/Documents/u3d-workdspace_5.0/slua-game-lib/others/slua/slua.c \
-  /Users/sanx/Documents/u3d-workdspace_5.0/slua-game-lib/others/lua-5.3.2/src/lua.h \
-  /Users/sanx/Documents/u3d-workdspace_5.0/slua-game-lib/others/lua-5.3.2/src/luaconf.h \
-  /Users/sanx/Documents/u3d-workdspace_5.0/slua-game-lib/others/lua-5.3.2/src/lauxlib.h
