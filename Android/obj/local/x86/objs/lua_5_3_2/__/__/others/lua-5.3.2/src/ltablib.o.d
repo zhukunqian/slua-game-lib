1e6e5fde450efa7c7f5738bd58d1702d8426fbde
sanx <zhukunqian@gmail.com>
2016-04-18 10:30:27 +0800

    Revert ""
    This reverts commit 37bf150007b15b10549d8db581e9dbfcc9b703f5.

---
+++ Android/obj/local/x86/objs/lua_5_3_2/__/__/others/lua-5.3.2/src/ltablib.o.d
@@ -0,0 +1,116 @@
+obj/local/x86/objs/lua_5_3_2/__/__/others/lua-5.3.2/src/ltablib.o: \
+ jni/../../others/lua-5.3.2/src/ltablib.c \
+ jni/../../others/lua-5.3.2/src/lprefix.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/limits.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/android/api-level.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/limits.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/limits.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/internal_types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/limits.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/syslimits.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/page.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/page_32.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/string.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/malloc.h \
+ jni/../../others/lua-5.3.2/src/lua.h \
+ jni/../../others/lua-5.3.2/src/luaconf.h \
+ jni/../../others/lua-5.3.2/src/lauxlib.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/stdio.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/stdint.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/_types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/_types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/_wchar_limits.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/posix_types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/stddef.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/compiler.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/posix_types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/kernel.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h \
+ jni/../../others/lua-5.3.2/src/lualib.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/time.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/time.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/time.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/siginfo.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm-generic/siginfo.h
+
+jni/../../others/lua-5.3.2/src/lprefix.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/limits.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/android/api-level.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/limits.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/limits.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/internal_types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/limits.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/syslimits.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/page.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/page_32.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/string.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/malloc.h:
+
+jni/../../others/lua-5.3.2/src/lua.h:
+
+jni/../../others/lua-5.3.2/src/luaconf.h:
+
+jni/../../others/lua-5.3.2/src/lauxlib.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/stdio.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/stdint.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/_wchar_limits.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/posix_types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/stddef.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/compiler.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/posix_types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/kernel.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h:
+
+jni/../../others/lua-5.3.2/src/lualib.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/time.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/time.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/time.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/siginfo.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm-generic/siginfo.h:
---
+++ Android/obj/local/x86/objs/lua_5_3_2/__/__/others/lua-5.3.2/src/lvm.o.d
@@ -0,0 +1,152 @@
+obj/local/x86/objs/lua_5_3_2/__/__/others/lua-5.3.2/src/lvm.o: \
+ jni/../../others/lua-5.3.2/src/lvm.c \
+ jni/../../others/lua-5.3.2/src/lprefix.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/limits.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/android/api-level.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/limits.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/limits.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/internal_types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/limits.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/syslimits.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/page.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/page_32.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/math.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/stdint.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/_types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/_types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/_wchar_limits.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/posix_types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/stddef.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/compiler.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/posix_types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/types.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/kernel.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/stdio.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/stdlib.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/string.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/malloc.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/alloca.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/strings.h \
+ D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/memory.h \
+ jni/../../others/lua-5.3.2/src/lua.h \
+ jni/../../others/lua-5.3.2/src/luaconf.h \
+ jni/../../others/lua-5.3.2/src/ldebug.h \
+ jni/../../others/lua-5.3.2/src/lstate.h \
+ jni/../../others/lua-5.3.2/src/lobject.h \
+ jni/../../others/lua-5.3.2/src/llimits.h \
+ jni/../../others/lua-5.3.2/src/ltm.h \
+ jni/../../others/lua-5.3.2/src/lzio.h \
+ jni/../../others/lua-5.3.2/src/lmem.h \
+ jni/../../others/lua-5.3.2/src/ldo.h \
+ jni/../../others/lua-5.3.2/src/lfunc.h \
+ jni/../../others/lua-5.3.2/src/lgc.h \
+ jni/../../others/lua-5.3.2/src/lopcodes.h \
+ jni/../../others/lua-5.3.2/src/lstring.h \
+ jni/../../others/lua-5.3.2/src/ltable.h \
+ jni/../../others/lua-5.3.2/src/lvm.h
+
+jni/../../others/lua-5.3.2/src/lprefix.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/limits.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/android/api-level.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/limits.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/limits.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/internal_types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/limits.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/syslimits.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/page.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/page_32.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/math.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/stdint.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/_wchar_limits.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/posix_types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/stddef.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/compiler.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/posix_types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/asm/types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/linux/types.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/machine/kernel.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/stdio.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/stdlib.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/string.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/malloc.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/alloca.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/strings.h:
+
+D:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/memory.h:
+
+jni/../../others/lua-5.3.2/src/lua.h:
+
+jni/../../others/lua-5.3.2/src/luaconf.h:
+
+jni/../../others/lua-5.3.2/src/ldebug.h:
+
+jni/../../others/lua-5.3.2/src/lstate.h:
+
+jni/../../others/lua-5.3.2/src/lobject.h:
+
+jni/../../others/lua-5.3.2/src/llimits.h:
+
+jni/../../others/lua-5.3.2/src/ltm.h:
+
+jni/../../others/lua-5.3.2/src/lzio.h:
+
+jni/../../others/lua-5.3.2/src/lmem.h:
+
+jni/../../others/lua-5.3.2/src/ldo.h:
+
+jni/../../others/lua-5.3.2/src/lfunc.h:
+
+jni/../../others/lua-5.3.2/src/lgc.h:
+
+jni/../../others/lua-5.3.2/src/lopcodes.h:
+
+jni/../../others/lua-5.3.2/src/lstring.h:
+
+jni/../../others/lua-5.3.2/src/ltable.h:
+
+jni/../../others/lua-5.3.2/src/lvm.h:
